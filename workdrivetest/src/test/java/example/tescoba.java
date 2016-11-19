0af0e00a52a4392520277a548976b9b7a645d141
monica <monica@IP-C6136007.corp.amazonworkspaces.com>
2016-11-18 19:17:41 -0800

    first commit 18 nov 16

---
+++ workdrivetest/src/test/java/example/tescoba.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class tescoba {
+	public String exePath = "D:\\Users\\monica\\Downloads\\chromedriver_win32\\chromedriver.exe";
+	public String url = "https://google.com";
+	// System.setProperty("webdriver.firefox.marionette", exePath);
+	// WebDriver driver = new FirefoxDriver();
+	public String urlChrome = System.setProperty("webdriver.chrome.driver", exePath);
+	public WebDriver driver = new ChromeDriver();
+
+  @Test
+  public void f() {
+	  
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		// Launch website
+		driver.navigate().to(url);
+		System.out.println("Application title is =============" + driver.getTitle());
+		// Maximize the browser
+		driver.manage().window().maximize();
+		System.out.println("success!!!");
+		
+
+	}
+}
