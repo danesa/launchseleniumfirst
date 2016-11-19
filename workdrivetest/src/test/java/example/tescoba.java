41302ab696616663c7fdd3470b461f5a4ad51824
monica <monica@IP-C6136007.corp.amazonworkspaces.com>
2016-11-18 20:27:39 -0800

    commit relase 2 18 nov 2016

--- workdrivetest/src/test/java/example/tescoba.java
+++ workdrivetest/src/test/java/example/tescoba.java
@@ -23,7 +23,7 @@ public class tescoba {
 		System.out.println("Application title is =============" + driver.getTitle());
 		// Maximize the browser
 		driver.manage().window().maximize();
-		System.out.println("success!!!");
+		System.out.println("success test!!!"+url);
 		
 
 	}
